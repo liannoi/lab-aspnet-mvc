19048821d2a41e08cb6e192487f4647fdd892eb9
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-02-27 16:19:17 +0200

    Domain layer described

---
+++ Src/Core/Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace HumanResources.Domain.Common
+{
+    public class BaseEntity
+    {
+        public string EntityId { get; set; }
+    }
+}
---
+++ Src/Core/Domain/Entities/JobTitleEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using HumanResources.Domain.Common;
+
+namespace HumanResources.Domain.Entities
+{
+    public class JobTitleEntity : BaseEntity
+    {
+        [Required] [StringLength(100)] public string Name { get; set; }
+
+        public ICollection<EmployeePromotionEntity> EmployeePromotions { get; set; }
+    }
+}
