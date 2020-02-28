95d4ea5b983504be7a2a83cabc27032c2e0e11be
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-02-28 12:17:08 +0200

    Added basic client project on MVC

---
+++ Src/Clients/WebUI/Controllers/JobController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HumanResources.Common.Repositories;
+
+namespace HumanResources.WebUI.Controllers
+{
+    public class JobController : Controller
+    {
+        private readonly JobTitleRepository _jobTitleRepository;
+
+        public JobController()
+        {
+            _jobTitleRepository = new JobTitleRepository();
+            _jobTitleRepository.Resolve();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(_jobTitleRepository.Result.Select());
+        }
+    }
+}
---
+++ Src/Clients/WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HumanResources.WebUI.MvcApplication" Language="C#" %>
